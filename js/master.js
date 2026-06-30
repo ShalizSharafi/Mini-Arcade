8eed6c7c9c16e0a1dac09d00a249f3c9230b3b0c
shalizsharafi <shalizoputh@gmail.com>
2026-06-30 11:32:54 -0700

    number guessing game almost completed

--- js/master.js
+++ js/master.js
@@ -2,7 +2,10 @@
 const navItem = document.querySelectorAll('.navItem')
 const numberGuesserBox = document.querySelector('.numberGuesserBox')
 const gameBox = document.querySelectorAll('.gamebox')
-
+const inp = document.getElementById('inp')
+const playBtn = document.querySelector('.play')
+const againBtn = document.querySelector('.again')
+const h3 = document.querySelector('.numberGuesserBox>h3')
 //clicking on the menu items and displaying the game chosen//
 
 //reset //
@@ -23,7 +26,7 @@ gameBox.forEach((val)=>{
 //reset //
 
 
-///number guesser game
+///number guesser game /////////////////////////******************************************************************************************************************************************
 //clicking on the menu items and displaying the game chosen//
 navItem[0].addEventListener('click',()=>{
        let temp = numberGuesserBox.getAttribute('data-status')
@@ -35,4 +38,52 @@ navItem[0].addEventListener('click',()=>{
               numberGuesserBox.style.display='none'
        }
 })
-//clicking on the menu items and displaying the game chosen//
+
+const randNum = parseInt(Math.random()*100)
+console.log(randNum)
+inp.setAttribute('disabled','disabled')
+
+let flag = 0
+let userGuess = 0
+playBtn.addEventListener('click',()=>{
+       inp.removeAttribute('disabled')
+       h3.style.animationPlayState='paused'
+       h3.innerText = 'guess remained: ' + Math.abs((flag-5))
+       if(inp.value != ''){
+              inp.style.border='none'
+              if(flag < 5){
+                     userGuess = inp.value
+                     if(userGuess < randNum){
+                            h3.innerText = 'the number is greater than your guess, you have ' + (Math.abs(5-flag)) + ' guess left'
+                     }else if(userGuess > randNum){
+                            h3.innerText = 'the number is lesser than your guess, you have ' + (Math.abs(5-flag)) + 'guess left'
+                     }else{
+                            h3.innerText = 'You won the game within  ' + (flag+1) +' try(s)! congratulations!!'
+                            h3.style.color ='rgb(129,166,95)'
+                            playBtn.style.backgroundColor ='#e8da9c'
+                            playBtn.style.color ='#1a1a2e'
+                            playBtn.innerText = 'You Won'
+                            playBtn.setAttribute('disabled','disabled')
+                     }
+                     flag++
+              }else{
+                     h3.innerText = 'You reached the number of guesses, yopu lost the game within 7 try(s)'
+                     inp.setAttribute('inert','inert')
+                     playBtn.style.backgroundColor ='#993c1d'
+                     playBtn.style.color= 'whitesmoke'
+                     playBtn.setAttribute('disabled','disabled')
+                     playBtn.innerText = 'You lost'
+              }
+       }else{
+              h3.innerText = 'empty input is NOT allowed'
+              inp.style.border='3px solid #993c1d'
+       }
+})
+//try again
+againBtn.addEventListener('click',()=>{
+       
+})
+//try again
+
+
+///number guesser ///////////////game******************************************************************************************************************************************
